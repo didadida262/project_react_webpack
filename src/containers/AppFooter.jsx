ff1d6c87a4806f8e0543157cfe08d11d8ddd318d
hhvcg <719713496@qq.com>
2022-08-25 21:59:01 +0800

    稍加修改

--- src/containers/AppFooter.jsx
+++ src/containers/AppFooter.jsx
@@ -1,6 +1,12 @@
+/*
+ * @Author: Hhvcg
+ * @Date: 2022-08-25 19:43:53
+ * @LastEditors: -_-
+ * @Description:
+ */
 import React from 'react'
 import { Layout } from 'antd'
 
 const { Footer } = Layout
 
-export default () => <Footer className='footer'>React Admin &copy;2019 Created By 游荡de蝌蚪</Footer>
+export default () => <Footer className='footer'>React Admin &copy;2021 Created By Hhvcg</Footer>
