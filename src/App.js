6937ae20e8a459b6b15bc2ae40236371c7f85b36
hhvcg <719713496@qq.com>
2023-07-08 03:10:02 +0800

    add: ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import styled from "styled-components"
+
+let LeftDiv = styled.div.attrs({
+  className: 'col-3 left-panel'
+})`
+  background-color: #7b8c7c;
+  min-height: 100vh;
+  padding: 0 10px;
+`
+let RightDiv = styled.div.attrs({
+  className: 'col-9 left-panel'
+
+})`
+  background-color: #c9d87c;
+  min-height: 100vh;
+  padding: 0 10px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>点歌系统</h1>
-      </header>
+    <div className="App container-fluid">
+      <div className='row'>
+        <LeftDiv> 左侧
+        </LeftDiv>
+        <RightDiv>右侧</RightDiv>
+      </div>
     </div>
   );
 }
