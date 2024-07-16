ce79eb5b6fc925455b084dcea970c27e65967cbe
didadida262 <719713496@qq.com>
2024-07-16 18:37:07 +0800

    add: xxx

--- src/pages/Botoperation/index.tsx
+++ src/pages/Botoperation/index.tsx
@@ -1,9 +1,17 @@
+/*
+ * @Description: 
+ * @Author: didadida262
+ * @Date: 2024-05-27 10:10:25
+ * @LastEditors: didadida262
+ * @LastEditTime: 2024-07-16 18:36:19
+ */
+import { Button} from 'antd';
+import { useContext } from "react"
 import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 import { useDispatch, useSelector  } from 'react-redux';
-import { useContext } from "react"
 
+import { ButtonCommon, EButtonType } from '../../components/ButtonCommon'
 import {ThemeContext, ThemeMode} from '../../components/themeProvider'
-import { Button} from 'antd';
 
 const LightDark = () => {
   const { currentTheme, setCurrentTheme } = useContext(ThemeContext)
@@ -14,9 +22,17 @@ const LightDark = () => {
   }
   return (
     <div className="h-[60px] px-2 py-2">
-      <Button icon={ currentTheme === 'Dark Mode'? <BsFillMoonFill />: <BsFillSunFill />} onClick={() => setCurrentTheme(currentTheme === 'Dark Mode'?ThemeMode.LIGHT_MODE: ThemeMode.DARK_MODE)}>
+      {/* <Button icon={ currentTheme === 'Dark Mode'? <BsFillMoonFill />: <BsFillSunFill />} onClick={() => setCurrentTheme(currentTheme === 'Dark Mode'?ThemeMode.LIGHT_MODE: ThemeMode.DARK_MODE)}>
        <span>{currentTheme}</span>
-      </Button>
+      </Button> */}
+      <ButtonCommon
+        type={EButtonType.SIMPLE}
+        onClick={() => setCurrentTheme(currentTheme === 'Dark Mode'?ThemeMode.LIGHT_MODE: ThemeMode.DARK_MODE)}>
+          { currentTheme === 'Dark Mode'? <BsFillMoonFill />: <BsFillSunFill />}
+          <span
+          className='ml-[8px]'
+        >{currentTheme}</span>
+      </ButtonCommon>
     </div>
     )
 }
