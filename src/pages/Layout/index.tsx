ee60e82ed13b740e6696419e0e0baf1db32df74d
didadida262 <719713496@qq.com>
2024-07-18 15:04:24 +0800

    add: ui改造

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -3,31 +3,31 @@
  * @Author: didadida262
  * @Date: 2024-03-14 00:47:07
  * @LastEditors: didadida262
- * @LastEditTime: 2024-07-18 14:11:36
+ * @LastEditTime: 2024-07-18 15:02:59
  */
 import React from 'react';
+
+import pattern from '@/styles/pattern';
 
 import LightDark from '../Botoperation';
 import ContentComponent from '../Content';
 import HeaderComponent from '../Header';
-import Menu from '../menu';
-
-import './index.scss';
+import Nav from '../menu';
 
 export const TextContext = React.createContext('测试数据');
 const LayoutComponent = () => {
   return (
     <TextContext.Provider value='dark'>
-      <div className='layout flex-col-sb markBorderG h-screen'>
-        <div className='header w-full h-[80px] border-b-[1px] border-b-solid border-b-borderSecondColor'>
+      <div className='layout flex-col-sb h-screen'>
+        <div className='header w-full h-[60px] border-b-[1px] border-b-solid border-b-borderSecondColor'>
           <HeaderComponent />
         </div>
-        <div className='view flex-sb h-[calc(100%_-_80px)] w-full '>
-          <div className=' sidebar flex flex-col justify-between border-r-[1px] border-r-solid border-r-borderSecondColor h-screen w-full'>
-            <Menu />
-            <LightDark />
+        <div className={`view h-[calc(100%_-_60px)] w-full ${pattern.flexbet}`}>
+          <div className='w-[220px] h-full border-r-[1px] border-solid border-borderSecondColor'>
+            <Nav className='w-full' />
+            {/* <LightDark /> */}
           </div>
-          <div className='w-[calc(100%_-_250px)] h-full'>
+          <div className='w-[calc(100%_-_220px)] h-full bg-[#000000] pt-[10px] px-[20px]'>
             <ContentComponent />
           </div>
         </div>
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,9 +1,9 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: didadida262
  * @Date: 2024-03-25 15:13:17
  * @LastEditors: didadida262
- * @LastEditTime: 2024-07-17 01:41:05
+ * @LastEditTime: 2024-07-18 14:51:42
  */
 import {
   AppstoreOutlined,
@@ -12,11 +12,12 @@ import {
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
-import React, {useContext} from 'react';
-import { BsNut,BsRocket, BsYinYang, BsRadioactive } from "react-icons/bs";
+import cn from 'classnames';
+import React, { useContext } from 'react';
+import { BsNut, BsRocket, BsYinYang, BsRadioactive } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 
-import {ThemeContext, ThemeMode} from '../../components/themeProvider'
+import { ThemeContext, ThemeMode } from '../../components/themeProvider';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -37,50 +38,61 @@ function getItem(
 }
 
 const items: MenuProps['items'] = [
-  getItem('Paperjs', 'paperjs', null, [
-    getItem('标注', 'label', <PieChartOutlined />),
-    getItem('Tank', 'tank', <AppstoreOutlined />),
-    getItem('Tadpoles', 'tadpoles', <ContainerOutlined/>)], 'group'),
-  getItem('Threejs', 'threejs', null, [
-    getItem('Test', 'threejs/threejstest',
-    <BsRocket />
-)
-  ], 'group'),
-  getItem('other', 'grp', null, [
-    getItem('Test', 'about', 
-    <BsNut />
-),
-    getItem('ChatGpt', 'ChatGpt',
-    <BsYinYang />
-    ),
-    getItem('TestCss', 'testcss',
-    <BsRadioactive />
-    ),
-    getItem('DiyComponents', 'diycomponent',
-    <BsRadioactive />
-    )
-  ], 'group'),
+  getItem(
+    'Paperjs',
+    'paperjs',
+    null,
+    [
+      getItem('标注', 'label', <PieChartOutlined />),
+      getItem('Tank', 'tank', <AppstoreOutlined />),
+      getItem('Tadpoles', 'tadpoles', <ContainerOutlined />),
+    ],
+    'group',
+  ),
+  getItem(
+    'Threejs',
+    'threejs',
+    null,
+    [getItem('Test', 'threejs/threejstest', <BsRocket />)],
+    'group',
+  ),
+  getItem(
+    'other',
+    'grp',
+    null,
+    [
+      getItem('Test', 'about', <BsNut />),
+      getItem('ChatGpt', 'ChatGpt', <BsYinYang />),
+      getItem('TestCss', 'testcss', <BsRadioactive />),
+      getItem('DiyComponents', 'diycomponent', <BsRadioactive />),
+    ],
+    'group',
+  ),
 ];
+interface IPropsNav {
+  className: string;
+}
 
-const App: React.FC = () => {
-  const { currentTheme } = useContext(ThemeContext)
+const Nav = (props: IPropsNav) => {
+  const { currentTheme } = useContext(ThemeContext);
+  const { className } = props;
 
-  const navigat = useNavigate()
+  const navigat = useNavigate();
   const onClick: MenuProps['onClick'] = (e) => {
     console.log('click ', e);
-    navigat(`/${e.key}`)
+    navigat(`/${e.key}`);
   };
   return (
     <Menu
       onClick={onClick}
-      className={`w-[250px] bg-[--bg-color]`}
+      className={cn(`w-[250px] bg-[--bg-color]`, className)}
       defaultSelectedKeys={['1']}
       defaultOpenKeys={['paperjs']}
-      mode="inline"
+      mode='inline'
       items={items}
-      theme={currentTheme === ThemeMode.LIGHT_MODE? 'light' : "dark"}
+      theme={currentTheme === ThemeMode.LIGHT_MODE ? 'light' : 'dark'}
     />
   );
 };
 
-export default App;
+export default Nav;
